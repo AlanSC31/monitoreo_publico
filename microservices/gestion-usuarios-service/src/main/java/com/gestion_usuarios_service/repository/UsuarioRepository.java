34f7ac36a1437cd445463d05e10eeb6ea4f7ada0
Alan <alan.sanchez3201@alumnos.udg.mx>
2025-06-09 22:44:20 -0600

    microservicio gestion de usuarios terminado, solo falta dockerizar

--- microservices/gestion-usuarios-service/src/main/java/com/gestion_usuarios_service/repository/UsuarioRepository.java
+++ microservices/gestion-usuarios-service/src/main/java/com/gestion_usuarios_service/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.gestion_usuarios_service.repository;
 
 import com.gestion_usuarios_service.model.Usuario;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    Usuario findByNombre(String nombre);
+    List<Usuario> findByStatusTrue();
 }
